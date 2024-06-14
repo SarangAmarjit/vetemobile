43c9493cfaf45f154546730e789652cef9167dde
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2024-06-14 22:17:52 +0530

    Updated Camera Ui Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:developer';
+import 'dart:typed_data';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:geocoding/geocoding.dart';
@@ -7,7 +9,6 @@ import 'package:geolocator/geolocator.dart';
 import 'dart:io';
 import 'package:image/image.dart' as img;
 import 'package:path_provider/path_provider.dart';
-import 'dart:typed_data';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -22,7 +23,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        fontFamily: 'KulimPark',
+        useMaterial3: true,
+      ),
       home: const TakePictureScreen(),
     );
   }
@@ -38,13 +42,21 @@ class TakePictureScreen extends StatefulWidget {
 class TakePictureScreenState extends State<TakePictureScreen> {
   final ImagePicker _picker = ImagePicker();
   final List<File?> _images = [null, null, null];
-
+  List<bool> isimageloaded = [false, false, false];
   Future<void> _pickImage(int index) async {
-    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    final pickedFile = await _picker.pickImage(
+        source: ImageSource.camera,
+        imageQuality: 30,
+        maxHeight: 2304,
+        maxWidth: 1300);
 
     if (pickedFile != null) {
+      setState(() {
+        isimageloaded[index] = true;
+      });
       final newPath = await _getAndEmbedLocation(pickedFile.path);
       setState(() {
+        isimageloaded[index] = false;
         _images[index] = File(newPath);
       });
     }
@@ -77,7 +89,11 @@ class TakePictureScreenState extends State<TakePictureScreen> {
         await getAddressFromLatLng(position.latitude, position.longitude);
     final gpsCoordinates =
         'Lat: ${position.latitude}, Lon: ${position.longitude}';
-    var vetewidth = getTextWidth(gpsCoordinates, img.arial48);
+    // final fontData = await rootBundle.load('assets/fonts/kulimpark3.zip');
+    // final font = img.BitmapFont.fromZip(fontData.buffer.asUint8List());
+    final font = img.arial48;
+
+    var vetewidth = getTextWidth(gpsCoordinates, font);
 
     img.Image originalImage =
         img.decodeImage(File(imagePath).readAsBytesSync())!;
@@ -86,26 +102,26 @@ class TakePictureScreenState extends State<TakePictureScreen> {
         originalImage: originalImage,
         address: address,
         yposition: 20,
-        vetewidth: null);
+        vetewidth: null,
+        font: font);
     drawStringAtRightCorner(
         originalImage: originalImage,
         address: gpsCoordinates,
         yposition: 100,
+        font: font,
         vetewidth: null);
     drawStringAtRightCorner(
         originalImage: originalImage,
         address: 'Vety',
         yposition: 180,
+        font: font,
         vetewidth: vetex);
 
     final directory = await getApplicationDocumentsDirectory();
     final newPath =
-        '${directory.path}/image_with_location_${DateTime.now().millisecondsSinceEpoch}.png';
+        '${directory.path}/image_with_location_${DateTime.now().millisecondsSinceEpoch}.jpg';
     File(newPath).writeAsBytesSync(
-      img.encodeJpg(
-        originalImage,
-        quality: 50,
-      ),
+      img.encodeJpg(originalImage, quality: 20, chroma: img.JpegChroma.yuv420),
     );
     return newPath;
   }
@@ -144,10 +160,8 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       {required img.Image originalImage,
       required String address,
       required int yposition,
-      required int? vetewidth}) async {
-    final fontData = await rootBundle.load('assets/fonts/kulimpark3.zip');
-    final font = img.BitmapFont.fromZip(fontData.buffer.asUint8List());
-
+      required int? vetewidth,
+      required img.BitmapFont font}) async {
     var textWidth = getTextWidth(address, font);
 
     int x = originalImage.width - textWidth - textWidth ~/ 2;
@@ -167,7 +181,12 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     // Request storage permissions if not already granted
     var status = await Permission.storage.status;
     if (!status.isGranted) {
-      status = await Permission.manageExternalStorage.request();
+      final plugin = DeviceInfoPlugin();
+      final android = await plugin.androidInfo;
+      log('Android SDK Version :${android.version.sdkInt}');
+      status = android.version.sdkInt < 30
+          ? await Permission.storage.request()
+          : await Permission.manageExternalStorage.request();
     }
 
     if (status.isGranted) {
@@ -175,6 +194,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
         if (image != null) {
           final Uint8List bytes = await image.readAsBytes();
           final result = await ImageGallerySaver.saveImage(bytes);
+
           log('Image saved to gallery: $result');
         }
       }
@@ -183,33 +203,108 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     }
   }
 
+  void _viewImageFullScreen(File image) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FullScreenImageScreen(image: image),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 234, 249, 234),
       appBar: AppBar(title: const Text('Take a picture')),
-      body: Column(
-        children: [
-          for (int i = 0; i < 3; i++)
-            GestureDetector(
-              onTap: () => _pickImage(i),
-              child: Container(
-                margin: const EdgeInsets.all(8.0),
-                width: double.infinity,
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  border: Border.all(color: Colors.black),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              for (int i = 0; i < 3; i++)
+                GestureDetector(
+                  onTap: _images[i] != null
+                      ? () => _viewImageFullScreen(_images[i]!)
+                      : () => _pickImage(i),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Card(
+                      elevation: 10,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 200,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: _images[i] == null
+                            ? isimageloaded[i]
+                                ? const Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Center(
+                                        child: SizedBox(
+                                            width: 50,
+                                            height: 50,
+                                            child: CircularProgressIndicator()),
+                                      ),
+                                    ],
+                                  )
+                                : Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const Icon(Icons.camera_alt, size: 50),
+                                      Text(
+                                        'Capture Image ${(i + 1).toString()}',
+                                        style: const TextStyle(fontSize: 17),
+                                      )
+                                    ],
+                                  )
+                            : ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child:
+                                    Image.file(_images[i]!, fit: BoxFit.cover)),
+                      ),
+                    ),
+                  ),
                 ),
-                child: _images[i] == null
-                    ? const Icon(Icons.camera_alt, size: 50)
-                    : Image.file(_images[i]!, fit: BoxFit.cover),
+              const SizedBox(
+                height: 10,
               ),
-            ),
-          ElevatedButton(
-            onPressed: _saveImagesToGallery,
-            child: const Text('Save to Gallery'),
+              ElevatedButton(
+                onPressed: _images.contains(null)
+                    ? null
+                    : () {
+                        _saveImagesToGallery().whenComplete(() {
+                          return ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  backgroundColor: Colors.green,
+                                  content:
+                                      Text('Images Downloaded Successfully')));
+                        });
+                      },
+                child: const Text('Save to Gallery'),
+              ),
+            ],
           ),
-        ],
+        ),
+      ),
+    );
+  }
+}
+
+class FullScreenImageScreen extends StatelessWidget {
+  final File image;
+
+  const FullScreenImageScreen({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Full Screen Image')),
+      body: Center(
+        child: Image.file(image),
       ),
     );
   }
